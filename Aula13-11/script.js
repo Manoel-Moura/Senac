e729f75c22646cdb6030a71ece0159fd61de1ebf
Manoel Moura <manoelmourabcc@gmail.com>
2023-11-14 21:25:54 -0300

    Criando card com JS

--- Aula13-11/script.js
+++ Aula13-11/script.js
@@ -1,10 +1,47 @@
-let container = document.getElementById("container")
+let container = document.getElementById("container");
 
-container.innerHTML = `<div class="card">
-<div class="top">
 
-</div>
-<div class="bottom">
+let fragment = document.createDocumentFragment();
 
-</div>
-</div>`
+let card = document.createElement('div');
+
+card.setAttribute('class','card');
+
+let div_img = document.createElement('div');
+
+div_img.setAttribute('class','div_img');
+
+card.append(div_img);
+
+let div_chef = document.createElement('div')
+div_chef.setAttribute('class', 'chef-avatar')
+
+card.append(div_chef);
+
+fragment.append(card);
+
+container.append(card)
+
+
+
+
+
+
+/*
+let texto = "Indice : "
+
+for (let i = 1; i <= 500; i++) {
+   let card = `<div class="card">
+                    <div class="top">
+                    
+                    ${texto + i}
+                    </div>
+                    <div class="bottom">
+
+                    </div>
+                    </div>`
+
+container.innerHTML += card // innerHTML tem falha de segurança e é muito mais lento!!!
+}
+*/
+

66b59befa072bd979636d11025a5c6ff42d2a6fd
Manoel Moura <manoelmourabcc@gmail.com>
2024-01-22 19:06:46 -0300

    Incluindo o projeto servidor semifinal

---
+++ ES6/ProjetoServerSemiFinal/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+import mongoose from 'mongoose';
+
+class App{
+    constructor(){
+        mongoose.connect('mongodb+srv://manoel:Ael7rUY6MO3NCyEf@projeto-senac.atkatyd.mongodb.net/?retryWrites=true&w=majority');
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    };
+    middlewares(){
+        this.server.use(express.json());
+    };
+    routes(){
+        this.server.use(routes);
+    };
+};
+
+module.exports = new App().server;
